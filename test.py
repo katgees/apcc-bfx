c3ef9038748282c77ff9e2d3cab53d2bd2de56a4
Kat Gees <katgees@live.com>
2022-05-10 10:47:21 -0700

    job ID related notes and changes

--- test.py
+++ test.py
@@ -3,7 +3,15 @@
 import requests
 
 def main():
-    print('main')
+
+    job_id = 'muscle-I20220509-233335-0712-47432951-p1m'
+    request = requests.get('https://www.ebi.ac.uk/Tools/services/rest/muscle/result/'+job_id+'/aln-clustalw')
+    print(request.text)
+
+    id_list = open('files/list.txt', 'a')
+    id_list.write(job_id+'\n')
+
+    id_list.close()
 
 if __name__ == '__main__':
     main()
